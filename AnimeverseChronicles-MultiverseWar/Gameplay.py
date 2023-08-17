7680c1d0265112ad7dd728eea1649d9f88da6dfe
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2023-08-17 18:54:00 +0700

    update gameplay

--- AnimeverseChronicles-MultiverseWar/Gameplay.py
+++ AnimeverseChronicles-MultiverseWar/Gameplay.py
@@ -6,36 +6,59 @@ from color import *
 class gameplay():
     def __init__(self):
         pygame.init()
-        self.bg = pygame.image.load('GameplayAssets\\bg0.jpg')
+        self.bg = pygame.image.load('GameplayAssets\\bg1.jpg')
+        self.path = pygame.image.load('GameplayAssets\\path1.png')
         self.nexus1 = pygame.image.load('GameplayAssets\\nexus1.png')
         self.nexus2 = pygame.image.load('GameplayAssets\\nexus2.png')
         self.board = pygame.image.load('GameplayAssets\\board.png')
+
         self.timer_font = pygame.font.Font('Fonts\\joystix_monospace.otf', 16)
         self.start_time = time.time()
+        self.curr_time = 0
+        self.tmp_time = 0
+        self.pause_time = 0
+        self.start_pause_time = 0
+
         self.gold_font = pygame.font.Font('Fonts\\joystix_monospace.otf', 20)
         self.curr_gold = 0
-        self.gold_count_time = time.time()
+        self.tmp_gold = 0
+        self.prev_gold_time = 0
+
+        self.play_button = pygame.image.load('GameplayAssets\\play_button.png')
+        self.pause_button = pygame.image.load('GameplayAssets\\pause_button.png')
+        self.isPlay = True
     
+    def SetScreen(self, screen):
+        self.screen = screen
+
+    def SwitchPlayPauseState(self):
+        if self.isPlay == True:
+            self.tmp_gold = self.curr_gold
+
+            self.tmp_time = self.curr_time
+            self.start_pause_time = time.time()
+        else:
+            self.pause_time += time.time() - self.start_pause_time
+
     def update(self):
         info = pygame.display.Info()
         self.bg = pygame.transform.smoothscale(self.bg, (info.current_w, info.current_h))
+        self.path = pygame.transform.smoothscale(self.path, (self.bg.get_rect().width, self.screen.get_rect().height // 7))
         pre_nexus_height = self.nexus1.get_rect().height
         pre_nexus_width = self.nexus1.get_rect().width
         new_nexus_width = info.current_w//10
         self.nexus1 = pygame.transform.smoothscale(self.nexus1, (new_nexus_width, new_nexus_width / pre_nexus_width * pre_nexus_height))
         self.nexus2 = pygame.transform.smoothscale(self.nexus2, (new_nexus_width, new_nexus_width / pre_nexus_width * pre_nexus_height))
-        self.board = pygame.transform.smoothscale(self.board, (240, 100))
+        self.board = pygame.transform.smoothscale(self.board, (self.screen.get_rect().width / 7, self.screen.get_rect().width / 7 / 2.4))
+        self.play_button = pygame.transform.smoothscale(self.play_button, (self.board.get_rect().width // 6, self.board.get_rect().width // 6))
+        self.pause_button = pygame.transform.smoothscale(self.pause_button, (self.board.get_rect().width // 6, self.board.get_rect().width // 6))
 
-        if time.time() - self.gold_count_time > 1:
-            self.curr_gold += 10
-            self.gold_count_time = time.time()
-        self.gold_text = self.gold_font.render(str(self.curr_gold), True, Yellow)
-        self.gold_text_rect = self.gold_text.get_rect()
-        self.gold_text_rect.center = (self.board.get_rect().width // 2, self.board.get_rect().height // 3)
-
-        curr_time = time.time() - self.start_time
-        curr_tmp_min = int(curr_time//60)
-        curr_tmp_sec = int(curr_time - (curr_time//60) * 60)
+        if self.isPlay == False:
+            self.curr_time = self.tmp_time
+        else:
+            self.curr_time = time.time() - self.start_time - self.pause_time
+        curr_tmp_min = int(self.curr_time//60)
+        curr_tmp_sec = int(self.curr_time - (self.curr_time//60) * 60)
         if curr_tmp_min < 10:
             curr_min = '0' + str(curr_tmp_min)
         else:
@@ -47,5 +70,17 @@ class gameplay():
         self.timer_text = self.timer_font.render(curr_min + ':' + curr_sec, True, Black)
         self.timer_text_rect = self.timer_text.get_rect()
         self.timer_text_rect.center = (self.board.get_rect().width // 2, self.board.get_rect().height // 3 * 2)
+
+        if self.curr_time - self.prev_gold_time >= 1:
+            self.curr_gold += 10
+            self.prev_gold_time = self.curr_time
+        if self.isPlay == False:
+            self.curr_gold = self.tmp_gold
+        self.gold_text = self.gold_font.render(str(self.curr_gold), True, Yellow)
+        self.gold_text_rect = self.gold_text.get_rect()
+        self.gold_text_rect.center = (self.board.get_rect().width // 2, self.board.get_rect().height // 3)
+
+        
+
 
         
--- AnimeverseChronicles-MultiverseWar/main.py
+++ AnimeverseChronicles-MultiverseWar/main.py
@@ -2,31 +2,54 @@ import pygame
 from pygame.locals import *
 from Gameplay import *
 
-def update():
-    Gameplay.update()
-
 if __name__ == '__main__':
     Gameplay = gameplay()
     pygame.init()
     screen = pygame.display.set_mode((1366, 768), RESIZABLE)
     pygame.display.set_caption('AnimeverseChronicles-MultiverseWar')
 
+    Gameplay.SetScreen(screen)
     Gameplay.update()
+
+    play_pause_pos = (screen.get_rect().width - Gameplay.pause_button.get_rect().width - 10, 10)
+    mouse = pygame.mouse.get_pos()
+
     pygame.display.update()
 
+def draw_gameplay_ui():
+    screen.blit(Gameplay.bg, (0, 0))
+    screen.blit(Gameplay.path, (0, screen.get_rect().height - Gameplay.path.get_rect().height))
+    screen.blit(Gameplay.nexus1, (5,  screen.get_rect().height - Gameplay.path.get_rect().height - Gameplay.nexus1.get_rect().height + Gameplay.path.get_rect().height // 3))
+    screen.blit(Gameplay.nexus2, (screen.get_rect().width - 5 - Gameplay.nexus2.get_rect().width,  screen.get_rect().height - Gameplay.path.get_rect().height - Gameplay.nexus1.get_rect().height + Gameplay.path.get_rect().height // 3)) 
+    screen.blit(Gameplay.board, (-2,  -2))
+    screen.blit(Gameplay.timer_text, Gameplay.timer_text_rect)
+    screen.blit(Gameplay.gold_text, Gameplay.gold_text_rect)
+    
+    if Gameplay.isPlay == True:
+        screen.blit(Gameplay.pause_button, play_pause_pos)
+    else:
+        screen.blit(Gameplay.play_button, play_pause_pos)
+
+def check_click():
+    if play_pause_pos[0] <= mouse[0] <= play_pause_pos[0] + Gameplay.pause_button.get_rect().width and play_pause_pos[1] <= mouse[1] <= play_pause_pos[1] + Gameplay.pause_button.get_rect().height:
+        Gameplay.SwitchPlayPauseState()
+        Gameplay.isPlay = 1 - Gameplay.isPlay
+        return
+
+if __name__ == '__main__':
     running = True
     while running:
-        update()
+        Gameplay.update()
+        mouse = pygame.mouse.get_pos()
+        play_pause_pos = (screen.get_rect().width - Gameplay.pause_button.get_rect().width - 10, 10)
+
         for event in pygame.event.get(): 
             if event.type == QUIT:
                 running = False
                 break
-        screen.blit(Gameplay.bg, (0, 0))
-        screen.blit(Gameplay.nexus1, (20,  510))
-        screen.blit(Gameplay.nexus2, (1206,  510))
-        screen.blit(Gameplay.board, (-2,  -2))
-        screen.blit(Gameplay.timer_text, Gameplay.timer_text_rect)
-        screen.blit(Gameplay.gold_text, Gameplay.gold_text_rect)
+            if event.type == MOUSEBUTTONDOWN:
+                check_click()
+        draw_gameplay_ui()
         pygame.display.update()
 
         # print(pygame.mouse.get_pos())
